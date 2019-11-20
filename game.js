fdd193db9f3b956a003d3649b706fe375c0ecb55
sh3ev <sheevron@gmail.com>
2019-11-20 02:40:42 +0100

    added moving foreground

--- game.js
+++ game.js
@@ -61,12 +61,20 @@ const fg = {
     x: 0,
     y: cvs.height - 112,
 
+    dx: 2,
+
     draw : function() {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
         this.w, this.h);
 
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
         this.w, this.h)
+    },
+
+    update: function(){
+        if(state.current == state.game){
+            this.x = (this.x - this.dx)%(this.w/2);
+        }
     }
 }
 // DRAGON
@@ -191,6 +199,7 @@ function draw(){
 
 function update() {
     dragon.update();
+    fg.update();
 }
 
 // LOOP
