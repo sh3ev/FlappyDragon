732ec94da0dcac556c9f00084f7c32078b904759
sh3ev <sheevron@gmail.com>
2019-11-21 00:55:13 +0100

    make code more readable, indent correction, comment etc.

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const ctx = cvs.getContext("2d");
 
 // GAME VARS AND CONSTS
 let frames = 0;
-const DEGREE = Math.PI/180;
+const DEGREE = Math.PI / 180;
 
 // LOAD SPRITE IMAGES
 const sprite = new Image();
@@ -35,362 +35,373 @@ MUSIC.volume = 0.5;
 MUSIC.play();
 // GAME STATE
 const state = {
-    current : 0,
-    getReady : 0,
-    game : 1,
-    over : 2
+	current: 0,
+	getReady: 0,
+	game: 1,
+	over: 2
 }
 
 //START BUTTON COORD
 const startBtn = {
-    x : 120,
-    y : 263,
-    w : 83,
-    h : 29
+	x: 120,
+	y: 263,
+	w: 83,
+	h: 29
 }
 
 // CONTROL THE GAME
-cvs.addEventListener("click", function(evt) {
-    switch(state.current) {
-        case state.getReady:
-            state.current = state.game;
-            SWOOSHING.play();
-            break;
-        case state.game:
-            dragon.flap();
-            FLAP.play();
-            break;
-        case state.over:
-            let rect = cvs.getBoundingClientRect();
-            let clickX  = evt.clientX - rect.left;
-            let clickY  = evt.clientY - rect.top;
+cvs.addEventListener("click", function (evt) {
+	switch (state.current) {
+		case state.getReady:
+			state.current = state.game;
+			SWOOSHING.play();
+			break;
+		case state.game:
+			dragon.flap();
+			FLAP.play();
+			break;
+		case state.over:
+			let rect = cvs.getBoundingClientRect();
+			let clickX = evt.clientX - rect.left;
+			let clickY = evt.clientY - rect.top;
 
-            // CHECK IF CLICK ON THE BUTTON
-            if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >=
-            startBtn.y && clickY <= startBtn.y + startBtn.h){
-                pipes.reset();
-                dragon.speedReset();
-                score.reset();
-                state.current = state.getReady;
-            }
-            break;
-    }
+			// CHECK IF CLICK ON THE BUTTON
+			if (clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >=
+				startBtn.y && clickY <= startBtn.y + startBtn.h) {
+				pipes.reset();
+				dragon.speedReset();
+				score.reset();
+				state.current = state.getReady;
+			}
+			break;
+	}
 });
 
 //  BACKGROUND
 const bg = {
-    sX: 0,
-    sY: 0,
-    w: 275,
-    h: 226,
-    x: 0,
-    y: cvs.height - 226,
+	sX: 0,
+	sY: 0,
+	w: 275,
+	h: 226,
+	x: 0,
+	y: cvs.height - 226,
 
-    draw : function(){
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
-        this.w, this.h);
+	draw: function () {
+		ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+			this.w, this.h);
 
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
-        this.w, this.h)
-    }
+		ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
+			this.w, this.h)
+	}
 
 }
 
 // FOREGROUND
 const fg = {
-    sX: 276,
-    sY: 0,
-    w: 224,
-    h: 112,
-    x: 0,
-    y: cvs.height - 112,
+	sX: 276,
+	sY: 0,
+	w: 224,
+	h: 112,
+	x: 0,
+	y: cvs.height - 112,
 
-    dx: 2,
+	dx: 2,
 
-    draw : function() {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
-        this.w, this.h);
+	draw: function () {
+		ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+			this.w, this.h);
 
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
-        this.w, this.h)
-    },
+		ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
+			this.w, this.h)
+	},
 
-    update: function(){
-        if(state.current == state.game){
-            this.x = (this.x - this.dx)%(this.w/2);
-        }
-    }
+	update: function () {
+		if (state.current == state.game) {
+			this.x = (this.x - this.dx) % (this.w / 2);
+		}
+	}
 }
 // DRAGON
 const dragon = {
-    animation : [
-        {sX: 446, sY : 112},
-        {sX: 446, sY : 203},
-        {sX: 446, sY : 171},
-        {sX: 446, sY : 203}
-    ],
-    x: 50,
-    y: 150,
-    w: 52,
-    h: 31,
+	animation: [{
+			sX: 446,
+			sY: 112
+		},
+		{
+			sX: 446,
+			sY: 203
+		},
+		{
+			sX: 446,
+			sY: 171
+		},
+		{
+			sX: 446,
+			sY: 203
+		}
+	],
+	x: 50,
+	y: 150,
+	w: 52,
+	h: 31,
 
-    radius : 12,
+	radius: 12,
 
-    frame: 0,
+	frame: 0,
 
-    gravity : 0.25,
-    jump : 4.6,
-    speed : 0,
-    rotation: 0,
+	gravity: 0.25,
+	jump: 4.6,
+	speed: 0,
+	rotation: 0,
 
-    draw : function() {
-        let dragon = this.animation[this.frame];
+	draw: function () {
+		let dragon = this.animation[this.frame];
 
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.rotation);
-        ctx.drawImage(sprite, dragon.sX, dragon.sY, this.w, this.h, -this.w/2, -this.w/2,
-        this.w, this.h);
+		ctx.save();
+		ctx.translate(this.x, this.y);
+		ctx.rotate(this.rotation);
+		ctx.drawImage(sprite, dragon.sX, dragon.sY, this.w, this.h, -this.w / 2, -this.w / 2,
+			this.w, this.h);
 
-        ctx.restore();
-    },
+		ctx.restore();
+	},
 
-    flap: function(){
-        this.speed = - this.jump;
-    },
+	flap: function () {
+		this.speed = -this.jump;
+	},
 
-    update: function(){
-        // IF THE GAME IS GET READY STATE, THE DRAGON MUST FLAP SLOWLY
-        this.period = state.current == state.getReady ? 10 : 5;
-        // WE INCREMENT THE FRAME BY 1, EACH PERIOD 
-        this.frame += frames%this.period == 0 ? 1 : 0;
-        // FRAME GOES FROM 0 TO 4. THEN AGAIN TO 0
-        this.frame = this.frame%this.animation.length;
+	update: function () {
+		// IF THE GAME IS GET READY STATE, THE DRAGON MUST FLAP SLOWLY
+		this.period = state.current == state.getReady ? 10 : 5;
+		// WE INCREMENT THE FRAME BY 1, EACH PERIOD 
+		this.frame += frames % this.period == 0 ? 1 : 0;
+		// FRAME GOES FROM 0 TO 4. THEN AGAIN TO 0
+		this.frame = this.frame % this.animation.length;
 
-        if(state.current == state.getReady){
-            this.y = 150;  // RESET TO START POSITION
-            this.rotation = 0 * DEGREE;
-        }else{
-            this.speed += this.gravity;
-            this.y += this.speed;
+		if (state.current == state.getReady) {
+			this.y = 150; // RESET TO START POSITION
+			this.rotation = 0 * DEGREE;
+		} else {
+			this.speed += this.gravity;
+			this.y += this.speed;
 
-            if(this.y + this.h/2 >= cvs.height - fg.h){
-                this.y = cvs.height - fg.h - this.h/2;
-                if(state.current == state.game){
-                    state.current = state.over; 
-                    DIE.play();
-                }
-            }
+			if (this.y + this.h / 2 >= cvs.height - fg.h) {
+				this.y = cvs.height - fg.h - this.h / 2;
+				if (state.current == state.game) {
+					state.current = state.over;
+					DIE.play();
+				}
+			}
 
-            // IF THE SPEED IS GREATER THAN JUMP MEANS THE BIRD IS FALLING DOWN
-            if(this.speed >= this.jump) {
-                this.rotation = 45 * DEGREE;
-                this.frame = 1;
-            }else{
-                this.rotation = -25 * DEGREE;
-            }
-        }
-    },
-    speedReset : function(){
-        this.speed = 0;
-    }
+			// IF THE SPEED IS GREATER THAN JUMP MEANS THE BIRD IS FALLING DOWN
+			if (this.speed >= this.jump) {
+				this.rotation = 45 * DEGREE;
+				this.frame = 1;
+			} else {
+				this.rotation = -25 * DEGREE;
+			}
+		}
+	},
+	speedReset: function () {
+		this.speed = 0;
+	}
 }
 
 // GET READY MESSAGE
 const getReady = {
-    sX : 0,
-    sY : 228,
-    w : 173,
-    h : 152,
-    x : cvs.width/2 -173/2,
-    y : 80,
+	sX: 0,
+	sY: 228,
+	w: 173,
+	h: 152,
+	x: cvs.width / 2 - 173 / 2,
+	y: 80,
 
-    draw: function(){
-        if(state.current == state.getReady){
-            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
-            this.w, this.h);
-        }
-    }
+	draw: function () {
+		if (state.current == state.getReady) {
+			ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+				this.w, this.h);
+		}
+	}
 
 }
 
 // GAME OVER MESSAGE
 const gameOver = {
-    sX : 175,
-    sY : 228,
-    w : 225,
-    h : 202,
-    x : cvs.width/2 -225/2,
-    y : 90,
+	sX: 175,
+	sY: 228,
+	w: 225,
+	h: 202,
+	x: cvs.width / 2 - 225 / 2,
+	y: 90,
 
-    draw: function(){
-        if(state.current == state.over){
-            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
-            this.w, this.h);
-        }
+	draw: function () {
+		if (state.current == state.over) {
+			ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+				this.w, this.h);
+		}
 
-    }
+	}
 
 }
 
 // PIPES
 const pipes = {
-    position: [],
+	position: [],
 
-    top: {
-        sX : 553,
-        sY : 0
-    },
-    bottom: {
-        sX : 502,
-        sY : 0
-    },
+	top: {
+		sX: 553,
+		sY: 0
+	},
+	bottom: {
+		sX: 502,
+		sY: 0
+	},
 
-    w : 53,
-    h : 400,
-    gap : 100,
-    maxYPos : -150,
-    dx : 2,
+	w: 53,
+	h: 400,
+	gap: 100,
+	maxYPos: -150,
+	dx: 2,
 
-    draw : function() {
-        for(let i =0; i < this.position.length; i++){
-            let p = this.position[i];
+	draw: function () {
+		for (let i = 0; i < this.position.length; i++) {
+			let p = this.position[i];
 
-            let topYPos = p.y;
-            let bottomYPos = p.y + this.h + this.gap;
+			let topYPos = p.y;
+			let bottomYPos = p.y + this.h + this.gap;
 
-            // top pipe
-            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos,
-            this.w, this.h);
+			// top pipe
+			ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos,
+				this.w, this.h);
 
-            // bottom pipe
-            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos,
-            this.w, this.h);
-        }
-    },
+			// bottom pipe
+			ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos,
+				this.w, this.h);
+		}
+	},
 
-    update: function(){
-        if(state.current !== state.game) return;
+	update: function () {
+		if (state.current !== state.game) return;
 
-        if(frames%100 == 0){
-            this.position.push({
-                x : cvs.width,
-                y: this.maxYPos * ( Math.random() + 1 )
-            });
-        }
-        for(let i = 0; i < this.position.length; i++){
-            let p = this.position[i];
+		if (frames % 100 == 0) {
+			this.position.push({
+				x: cvs.width,
+				y: this.maxYPos * (Math.random() + 1)
+			});
+		}
+		for (let i = 0; i < this.position.length; i++) {
+			let p = this.position[i];
 
-            let bottomPipesYPos = p.y + this.h + this.gap;
+			let bottomPipesYPos = p.y + this.h + this.gap;
 
-            // COLLISION DETECTION
-            // TOP PIPE
-            if(dragon.x + dragon.radius > p.x && dragon.x - dragon.radius < p.x + this.w && 
-            dragon.y + dragon.radius > p.y && dragon.y - dragon. radius < p.y + this.h){
-                state.current = state.over;
-                HIT.play();
-            }
-            // BOTTOM PIPE
-            if(dragon.x + dragon.radius > p.x && dragon.x - dragon.radius < p.x + this.w && 
-            dragon.y + dragon.radius > bottomPipesYPos && dragon.y - dragon. radius < bottomPipesYPos + this.h){
-                state.current = state.over;
-                HIT.play();
-            }
+			// COLLISION DETECTION
+			// TOP PIPE
+			if (dragon.x + dragon.radius > p.x && dragon.x - dragon.radius < p.x + this.w &&
+				dragon.y + dragon.radius > p.y && dragon.y - dragon.radius < p.y + this.h) {
+				state.current = state.over;
+				HIT.play();
+			}
+			// BOTTOM PIPE
+			if (dragon.x + dragon.radius > p.x && dragon.x - dragon.radius < p.x + this.w &&
+				dragon.y + dragon.radius > bottomPipesYPos && dragon.y - dragon.radius < bottomPipesYPos + this.h) {
+				state.current = state.over;
+				HIT.play();
+			}
 
-            //MOVE THE PIPES TO THE LEFT
-            p.x -= this.dx;
+			//MOVE THE PIPES TO THE LEFT
+			p.x -= this.dx;
 
-            // if the pipes fo beyond canvas, delete them from array
-            if(p.x + this.w <= 0){
-                this.position.shift();
-                score.value += 1;
-                POINT.play();
-                setRandomColor();
-                score.best = Math.max(score.value, score.best);
-                localStorage.setItem("best", score.best);
-            }
-        }
-    },
-    
-    reset : function(){
-        this.position = [];
-    }
+			// if the pipes fo beyond canvas, delete them from array
+			if (p.x + this.w <= 0) {
+				this.position.shift();
+				score.value += 1;
+				POINT.play();
+				setRandomColor();
+				score.best = Math.max(score.value, score.best);
+				localStorage.setItem("best", score.best);
+			}
+		}
+	},
+
+	reset: function () {
+		this.position = [];
+	}
 }
 
 // SCORE
 const score = {
-    best: parseInt(localStorage.getItem("best")) || 0,
-    value: 0,
+	best: parseInt(localStorage.getItem("best")) || 0,
+	value: 0,
 
-    draw : function(){
-        ctx.fillStyle = "#FFF";
-        ctx.strokeStyle = "#000";
+	draw: function () {
+		ctx.fillStyle = "#FFF";
+		ctx.strokeStyle = "#000";
 
-        if(state.current == state.game){
-            ctx.lineWidth = 2;
-            ctx.font = "35px Teko";
-            ctx.fillText(this.value, cvs.width/2, 50);
-            ctx.strokeText(this.value, cvs.width/2, 50);
-        }else if(state.current == state.over) {
-            // SCORE VALUE
-            ctx.font = "25px Teko";
-            ctx.fillText(this.value, 225, 186);
-            ctx.strokeText(this.value, 225, 186);
-            // BEST SCORE
-            ctx.fillText(this.best, 225, 228);
-            ctx.strokeText(this.best, 225, 228);
-        }
-    },
+		if (state.current == state.game) {
+			ctx.lineWidth = 2;
+			ctx.font = "35px Teko";
+			ctx.fillText(this.value, cvs.width / 2, 50);
+			ctx.strokeText(this.value, cvs.width / 2, 50);
+		} else if (state.current == state.over) {
+			// SCORE VALUE
+			ctx.font = "25px Teko";
+			ctx.fillText(this.value, 225, 186);
+			ctx.strokeText(this.value, 225, 186);
+			// BEST SCORE
+			ctx.fillText(this.best, 225, 228);
+			ctx.strokeText(this.best, 225, 228);
+		}
+	},
 
-    reset : function(){
-        this.value = 0;
-    }
+	reset: function () {
+		this.value = 0;
+	}
 }
 
 // DRAW
-function draw(){
-    ctx.fillStyle = "#190641";
-    ctx.fillRect(0, 0, cvs.width, cvs.height);
+function draw() {
+	ctx.fillStyle = "#190641";
+	ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-    bg.draw();
-    pipes.draw();
-    fg.draw();
-    dragon.draw();
-    getReady.draw();
-    gameOver.draw();
-    score.draw();
+	bg.draw();
+	pipes.draw();
+	fg.draw();
+	dragon.draw();
+	getReady.draw();
+	gameOver.draw();
+	score.draw();
 }
 
 // UPDATE
 
 function update() {
-    dragon.update();
-    fg.update();
-    pipes.update();
+	dragon.update();
+	fg.update();
+	pipes.update();
 }
 
 // LOOP
 
-function loop () {
-    update();
-    draw();
-    frames++;
+function loop() {
+	update();
+	draw();
+	frames++;
 
-    requestAnimationFrame(loop);
+	requestAnimationFrame(loop);
 }
 
 // SET RANDOM BACKGROUND COLOR
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  
-  
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+
+
 function setRandomColor() {
-    $("body").css("background-color", getRandomColor());
-  }
+	$("body").css("background-color", getRandomColor());
+}
 loop();
