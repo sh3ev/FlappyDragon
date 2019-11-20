e19f23dfc6a01e44fdf9f15c985ae1c1df2eede4
sh3ev <sheevron@gmail.com>
2019-11-20 01:18:03 +0100

    added background, and dragon animation

--- game.js
+++ game.js
@@ -1,14 +1,81 @@
 // SELECT CVS
-const cvs = document.getElementById("bird");
+const cvs = document.getElementById("dragon");
 const ctx = cvs.getContext("2d");
 
 // GAME VARS AND CONSTS
 let frames = 0;
 
+// LOAD SPRITE IMAGES
+const sprite = new Image();
+sprite.src = 'img/sprite.png';
+
+//  BACKGROUND
+
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+        this.w, this.h);
+
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
+        this.w, this.h)
+    }
+
+}
+
+// FOREGROUND
+const fg = {
+    sX: 276,
+    sY: 0,
+    w: 224,
+    h: 112,
+    x: 0,
+    y: cvs.height - 112,
+
+    draw : function() {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y,
+        this.w, this.h);
+
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y,
+        this.w, this.h)
+    }
+}
+// DRAGON
+const dragon = {
+    animation : [
+        {sX: 446, sY : 112},
+        {sX: 446, sY : 203},
+        {sX: 446, sY : 171},
+        {sX: 446, sY : 203}
+    ],
+    x: 50,
+    y: 150,
+    w: 52,
+    h: 31,
+
+    frame: 0,
+
+    draw : function() {
+        let dragon = this.animation[this.frame];
+        ctx.drawImage(sprite, dragon.sX, dragon.sY, this.w, this.h, this.x-this.w/2, this.y-this.w/2,
+        this.w, this.h);
+    }
+}
+
 // DRAW
 function draw(){
     ctx.fillStyle = "#70cbce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+
+    bg.draw();
+    fg.draw();
+    dragon.draw();
 }
 
 // UPDATE
